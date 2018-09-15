261ee52eb0a1abd447407413f393439e65e3d7bb
Akshit Dhar <akshitdhar@live.com>
2018-09-16 00:30:11 +0530

    AngularJs Template using gulp

---
+++ src/js/controllers/HomeCtrl.js
@@ -0,0 +1,7 @@
+angular.module('adres')
+.controller('HomeCtrl', [
+    '$scope', 
+    function($scope){
+        $scope.message = "Test";
+    }
+])
---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+angular.module('adres', [
+    'ngRoute'
+])
+.config([
+    '$routeProvider',
+    function($routeProvider){
+        $routeProvider
+            .when('/',{
+                templateUrl: 'views/home.html',
+                controller: 'HomeCtrl',
+                controllerAs: 'homeCtrl'
+            })
+    }
+])
+
