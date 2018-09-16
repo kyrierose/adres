dfbfcd05fda4e781d310d5aced5fa44024b23845
Akshit Dhar <akshitdhar@live.com>
2018-09-16 13:16:07 +0530

    Updated gulpfile with uglify, less support, clean build

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
+const less = require('gulp-less');
+const del = require('del');
+const uglify = require('gulp-uglify');
 
 const scripts = require('./scripts');
 const styles = require('./styles');
 
 var devMode = false;
 
-gulp.task('css', function(){
+gulp.task('less', function(){
     gulp.src(styles)
+        .pipe(less())
         .pipe(concat('main.css'))
         .pipe(gulp.dest('./dist/css'))
         .pipe(browserSync.reload({
@@ -19,6 +23,7 @@ gulp.task('css', function(){
 gulp.task('js', function(){
     gulp.src(scripts)
         .pipe(concat('scripts.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('./dist/js'))
         .pipe(browserSync.reload({
             stream:true
@@ -34,7 +39,7 @@ gulp.task('html', function(){
 });
 
 gulp.task('build', function(){
-    gulp.start(['css','html','js']);
+    gulp.start(['less','html','js']);
 });
 
 gulp.task('browser-sync', function(){
@@ -46,10 +51,14 @@ gulp.task('browser-sync', function(){
     });
 });
 
+gulp.task('clean', function(){
+    del.sync(['./build/**']);
+});
+
 gulp.task('start', function(){
     devMode = true;
-    gulp.start(['build', 'browser-sync']);
-    gulp.watch(['./src/css/**/*.css'], ['css']);
+    gulp.start(['clean', 'build', 'browser-sync']);
+    gulp.watch(['./src/less/**/*.less'], ['less']);
     gulp.watch(['./src/templates/**/*.html'], ['html']);
     gulp.watch(['./src/js/**/*.js'], ['js']);
 })
